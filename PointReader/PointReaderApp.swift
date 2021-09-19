09834eb02a917a101c9384be2140ce296a3cf408
castlele <castlelecs@gmail.com>
2021-09-19 09:23:18 +0300

    Initial Commit

---
+++ PointReader/PointReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointReaderApp.swift
+//  PointReader
+//
+//  Created by Nikita Semenov on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct PointReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
